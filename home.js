831bb2dda9f2ce8f6c56171d275ce37e2778161d
felipehmazotti <126676451+felipehmazotti@users.noreply.github.com>
2024-11-08 10:22:27 -0300

    commit

--- home.js
+++ home.js
@@ -57,3 +57,15 @@ next.addEventListener('click', handleScrollNext)
 prev.addEventListener('click', handleScrollPrev)
 
 
+$(".custom-carousel").owlCarousel({
+    autoWidth: true,
+    loop: true
+  });
+  $(document).ready(function () {
+    $(".custom-carousel .item").click(function () {
+      $(".custom-carousel .item").not($(this)).removeClass("active");
+      $(this).toggleClass("active");
+    });
+  });
+  
+
